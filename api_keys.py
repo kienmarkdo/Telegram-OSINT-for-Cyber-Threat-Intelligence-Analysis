3e900e52630504eb949d36df893b0bafa608011f
Kien Do <kienmarkdo@gmail.com>
2024-01-26 17:17:10 -0500

    Added api_keys file

---
+++ api_keys.py
@@ -0,0 +1,9 @@
+"""
+Replace the value None with your info. If it is a string, place the string in double-quotes ""
+"""
+USERNAME: str = None
+PHONE_NUMBER: str = None  # "+12345554444"
+API_KEY: str = None  # for Telegram Bot
+HTTP_API: str = None
+API_HASH: str = None
+API_ID: int = None
